e835178da877eab5ea0225afcdb27112e97523cc
Italo Gustavo Miranda Melo <italogmm@gmail.com>
2018-11-02 09:26:27 -0300

    Continuação - curso angular
    Continuação - curso angular

---
+++ meat-app-starter-master/src/app/about/about.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from "@angular/core";
+import { AboutComponent } from "./about.component";
+import { Routes, Router, RouterModule } from "@angular/router";
+
+const ROUTES: Routes = [
+    {path: '', component: AboutComponent}
+]
+
+@NgModule({
+    declarations: [AboutComponent],
+    imports: [
+        RouterModule,
+        RouterModule.forChild(ROUTES)
+    ]
+})
+export class AboutModule {
+
+}
--- meat-app-starter-master/src/app/order/order.component.ts
+++ meat-app-starter-master/src/app/order/order.component.ts
@@ -4,7 +4,7 @@ import { OrderService } from './order.service';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 import { Order, OrderItem } from './order.model';
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder } from '../../../node_modules/@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '../../../node_modules/@angular/forms';
 
 @Component({
   selector: 'mt-order',
@@ -16,11 +16,27 @@ export class OrderComponent implements OnInit {
 
   delivery: number = 8;
 
+  emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  numberPattern = /^[0-9]*$/
+
   paymentOptions: RadioOption[] = [
     {label: 'Dinheiro', value: 'MON'},
     {label: 'Cartão de débito', value: 'DEB'},
     {label: 'Cartão refeição', value: 'REF'}
   ];
+
+  static equalsTo(group: AbstractControl): {[key: string]: boolean} {
+    const email = group.get('email');
+    const emailConfirmation = group.get('emailConfirmation');
+    if (!email || !emailConfirmation) {
+      return undefined;
+    }
+
+    if (email.value !== emailConfirmation.value) {
+      return { emailsNotMatch: true };
+    }
+    return undefined;
+  }
 
   constructor(private orderService: OrderService, 
               private router: Router,
@@ -29,14 +45,14 @@ export class OrderComponent implements OnInit {
 
   ngOnInit() {
       this.orderForm = this.formBuilder.group({
-          name: this.formBuilder.control(''),
-          email: this.formBuilder.control(''),
-          emailConfirmation: this.formBuilder.control(''),
-          address: this.formBuilder.control(''),
-          number: this.formBuilder.control(''),
+          name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+          email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+          emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+          address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+          number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
           optionalAddress: this.formBuilder.control(''),
-          paymentOption: this.formBuilder.control('')
-      });
+          paymentOption: this.formBuilder.control('', [Validators.required])
+      }, {validator: OrderComponent.equalsTo});
   }
 
   itemsValue(): number {
